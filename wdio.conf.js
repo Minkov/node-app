c1131662ce2b97642e69dead6c26be09a60b4671
Doncho Minkov <DonchoMinkov@gmail.com>
2017-06-15 18:44:21 +0300

    Now testing on phantomjs

--- wdio.conf.js
+++ wdio.conf.js
@@ -39,10 +39,10 @@ exports.config = {
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: [
-    {
-        maxInstances: 5,
-        browserName: 'chrome',
-    }],
+        {
+            maxInstances: 5,
+            browserName: 'phantomjs',
+        }],
     //
     // ===================
     // Test Configurations
@@ -103,7 +103,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],//
+    services: ['phantomjs'],
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: http://webdriver.io/guide/testrunner/frameworks.html
